0a0f9edd3ae4a92bc6d8c658ac5fa1c8b975128e
TomTomKinch <kinchthomas@gmail.com>
2020-06-17 18:53:59 -0700

    Fixed Header

--- iff-app/src/App.js
+++ iff-app/src/App.js
@@ -3,22 +3,29 @@ import logo from './Images/IsForFunLogoCircleSmall.png';
 import ham_icon from './Images/hamburg_icon_thin.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ham_icon} classname="ham-icon" />
-        <img src={logo} className="IFF-logo" alt="logo" />
-        <h className="Title"> Is For Fun Podcast</h>
-      </header>
-      <div>
-        <p>This is where the good stuff will go</p>
+class App extends React.Component {
+  DropDown = () => {
+    console.log("DropDown");
+  } 
+  render() {
+    return (
+      <div className="App">
+        <header id="header" className="App-header">
+          <img className="Ham-icon" 
+            src={ham_icon} alt="ham" 
+            onClick={this.DropDown}/>
+          <img src={logo} className="IFF-logo" alt="logo" />
+          <p className="Title">Is For Fun Podcast</p>
+        </header>
+        <div>
+          <p>This is where the good stuff will go</p>
+        </div>
+        <footer className="App-footer">
+          <div className="Footer-info"> The Bottom of the Page</div>
+        </footer>
       </div>
-      <footer className="App-footer">
-        <h className="footer-info"> The Bottom of the Page</h>
-      </footer>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
