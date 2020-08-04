29def25fb9a728014a1d7608ed09e518fa1a24ba
TomTomKinch <kinchthomas@gmail.com>
2020-08-03 20:35:36 -0700

    Updates

---
+++ precache-manifest.2eeae75e09b338a3167657866c844e9e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a81edc78da556f118edc63394789757",
+    "url": "/IsForFunWebsite/index.html"
+  },
+  {
+    "revision": "acf06cfa85554aa308fc",
+    "url": "/IsForFunWebsite/static/css/main.00329628.chunk.css"
+  },
+  {
+    "revision": "ba10006b08f7573a1181",
+    "url": "/IsForFunWebsite/static/js/2.f35de338.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/IsForFunWebsite/static/js/2.f35de338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acf06cfa85554aa308fc",
+    "url": "/IsForFunWebsite/static/js/main.6051316e.chunk.js"
+  },
+  {
+    "revision": "265911f28b673d4c878c",
+    "url": "/IsForFunWebsite/static/js/runtime-main.d9a7eed9.js"
+  },
+  {
+    "revision": "6b99e940764926ea34bf19d6233dd364",
+    "url": "/IsForFunWebsite/static/media/IsForFunLogoCircleSmall.6b99e940.png"
+  },
+  {
+    "revision": "955dadff4370d4643af0abe4d37f49f4",
+    "url": "/IsForFunWebsite/static/media/burger.955dadff.png"
+  },
+  {
+    "revision": "6da53564d67fb0b278f5c8238d3c5273",
+    "url": "/IsForFunWebsite/static/media/mic.6da53564.png"
+  }
+]);
